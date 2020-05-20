e3986b307e2d4d1075c9e57db6838df6566b28f9
Rafael Gomes <rafarfl2010@gmail.com>
2020-05-20 13:51:06 -0300

    styling a progressbar

--- snippets/src/app/app.module.ts
+++ snippets/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule }   from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { ProgressBarComponent } from './progress-bar/progress-bar.component';
@@ -10,7 +11,8 @@ import { ProgressBarComponent } from './progress-bar/progress-bar.component';
     ProgressBarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- snippets/src/app/progress-bar/progress-bar.component.ts
+++ snippets/src/app/progress-bar/progress-bar.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit, Input } from '@angular/core';
 @Component({
   selector: 'app-progress-bar',
   templateUrl: './progress-bar.component.html',
-  styleUrls: ['./progress-bar.component.css']
+  styleUrls: ['./progress-bar.component.scss']
 })
 export class ProgressBarComponent implements OnInit {
 
@@ -15,7 +15,30 @@ export class ProgressBarComponent implements OnInit {
   constructor() { }  
 
   ngOnInit(): void {
-    
+   
+    if(!this.progress) {
+      this.progress = 0;
+    }
+
+    if(this.total === 0) {
+      this.total = this.progress;
+    } else if(!this.total) {
+      this.total = 100;
+    }
+
+    if(this.progress > this.total) {
+      this.progress = 100;
+      this.total = 100;
+    }
+    this.progress = (this.progress / this.total) * 100;
+
+    if(this.progress < 55) {
+      this.color = 'red';
+    } else if(this.progress < 75) {
+      this.color= 'yellow';
+    } else {
+    this.color = 'green';
+    }
   }
 
 }
